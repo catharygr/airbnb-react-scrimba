bca5bff14650558e5342a24ba2f05e4f390a6c18
cathy <cathy@bubulazi.com>
2023-01-10 16:26:54 +0000

    Trabajando en la navegación, componentes e imagenes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './App.css'
+import Navegacion from './componentes/Navegacion'
+
+function App() {
+  
+
+  return (
+    <main>
+      <Navegacion />
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/componentes/Navegacion.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logoAirbnb from '../images/airbnb-logo.png'
+
+export default function Navegacion() {
+  return(
+    <div>
+      <img src={logoAirbnb} alt='Logo de airbnb' />
+    </div>
+  )
+}
